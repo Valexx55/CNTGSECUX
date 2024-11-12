059593048f1643d1abc142e02f8a12432b495375
valexx55 <valexx55@gmail.com>
2024-11-12 18:55:48 +0100

    Initial commit

---
+++ app/src/main/java/edu/vmg/cntgsecux/MainActivity.kt
@@ -0,0 +1,26 @@
+package edu.vmg.cntgsecux
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.vmg.cntgsecux.permisosbasicos.ActividadUno
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val intent = Intent (this, ActividadUno::class.java)
+        //"navego a la actividad 1"
+        startActivity(intent) //lanzo la actividad destino
+    }
+}
---
+++ app/src/main/java/edu/vmg/cntgsecux/permisosbasicos/ActividadUno.kt
@@ -0,0 +1,21 @@
+package edu.vmg.cntgsecux.permisosbasicos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.vmg.cntgsecux.R
+
+class ActividadUno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_actividad_uno)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
